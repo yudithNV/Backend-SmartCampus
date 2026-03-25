933060ebc1ee8abea4e446f1315340d69fa1e6c5
IblingMarino666 <144977508+IblingMarino666@users.noreply.github.com>
2026-03-25 08:29:12 -0400

    correccion
    correccion

--- src/main/java/com/example/smartcampus/service/AuthService.java
+++ src/main/java/com/example/smartcampus/service/AuthService.java
@@ -20,7 +20,7 @@ public class AuthService {
     public LoginResponseDTO login(LoginRequestDTO request){
 
         User user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+                .orElseThrow(() -> new RuntimeException("correo o Contraseña incorrecta"));
 
         if(!passwordEncoder.matches(request.getPassword(), user.getPasswordHash())){
             throw new RuntimeException("correo o Contraseña incorrecta");
